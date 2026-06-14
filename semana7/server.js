2b16ad9059d7a4875d88334a59bea9dc99a73656
Sofia Vergara <leonorsofia888@gmail.com>
2026-06-13 22:58:48 -0500

    Semana 7

---
+++ semana7/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let reportes = [];
+
+// GET - consultar reportes
+app.get('/reportes', (req, res) => {
+  res.json(reportes);
+});
+
+// POST - crear reporte
+app.post('/reportes', (req, res) => {
+  const reporte = {
+    id: reportes.length + 1,
+    tipo: req.body.tipo,
+    descripcion: req.body.descripcion
+  };
+
+  reportes.push(reporte);
+
+  res.json({
+    mensaje: "Reporte registrado",
+    reporte: reporte
+  });
+});
+
+// iniciar servidor
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
